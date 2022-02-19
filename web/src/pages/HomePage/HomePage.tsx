79e855c793a2412a18499728f778385f12f3f011
Tobbe Lundberg <tobbe@tlundberg.com>
2022-02-19 21:37:31 +0100

    Mobile menu

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { useAuth } from '@redwoodjs/auth'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
@@ -12,6 +13,8 @@ const navigation = [
 ]
 
 const HomePage = () => {
+  const { isAuthenticated } = useAuth()
+
   return (
     <>
       <MetaTags title="Home" description="Home page" />
@@ -62,101 +65,57 @@ const HomePage = () => {
           </svg>
         </div>
         <div className="relative pt-6 pb-16 sm:pb-24 lg:pb-32">
-          <Popover>
-            <nav
-              className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
-              aria-label="Global"
-            >
-              <div className="flex items-center flex-1">
-                <div className="flex items-center justify-between w-full md:w-auto">
-                  <Link to={routes.home()}>
-                    <span className="sr-only">Rules</span>
-                    <img
-                      className="h-8 w-auto sm:h-10"
-                      src="https://d33wubrfki0l68.cloudfront.net/492ed629970792d32ac857da0166a7d2308bad99/428b6/images/diecut.svg"
-                      alt=""
-                    />
-                  </Link>
-                  <div className="-mr-2 flex items-center md:hidden">
-                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                      <span className="sr-only">Open main menu</span>
-                      <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                    </Popover.Button>
-                  </div>
-                </div>
-                <div className="hidden md:block md:ml-10 md:space-x-10">
-                  {navigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      to={routes[item.page]()}
-                      className="font-medium text-gray-500 hover:text-gray-900"
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
+          <nav
+            className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
+            aria-label="Global"
+          >
+            <div className="flex items-center flex-1">
+              <div className="flex items-center justify-between w-full w-auto">
+                <Link to={routes.home()}>
+                  <span className="sr-only">Home</span>
+                  <img
+                    className="h-8 w-auto sm:h-10"
+                    src="https://d33wubrfki0l68.cloudfront.net/492ed629970792d32ac857da0166a7d2308bad99/428b6/images/diecut.svg"
+                    alt=""
+                  />
+                </Link>
               </div>
-              <div className="hidden md:block text-right">
+              <div className="ml-5 space-x-5 md:ml-10 md:space-x-10">
+                <Link
+                  to={routes.rules()}
+                  className="font-medium text-gray-500 hover:text-gray-900"
+                >
+                  Rules
+                </Link>
+                <a
+                  href="#leader-board"
+                  className="font-medium text-gray-500 hover:text-gray-900"
+                >
+                  Leader board
+                </a>
+                {isAuthenticated && (
+                  <Link
+                    to={routes.profile()}
+                    className="font-medium text-gray-500 hover:text-gray-900"
+                  >
+                    Profile
+                  </Link>
+                )}
+              </div>
+            </div>
+            {!isAuthenticated && (
+              <div className="text-right">
                 <span className="inline-flex rounded-md shadow-md ring-1 ring-black ring-opacity-5">
                   <Link
-                    to="login"
+                    to={routes.home()}
                     className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50"
                   >
                     Log In
                   </Link>
                 </span>
               </div>
-            </nav>
-            <Transition
-              as={Fragment}
-              enter="duration-150 ease-out"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="duration-100 ease-in"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Popover.Panel
-                focus
-                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-              >
-                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div className="px-5 pt-4 flex items-center justify-between">
-                    <div>
-                      <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt=""
-                      />
-                    </div>
-                    <div className="-mr-2">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        <span className="sr-only">Close main menu</span>
-                        <XIcon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
-                    </div>
-                  </div>
-                  <div className="px-2 pt-2 pb-3 space-y-1">
-                    {navigation.map((item) => (
-                      <Link
-                        key={item.name}
-                        to={routes[item.page]()}
-                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                      >
-                        {item.name}
-                      </Link>
-                    ))}
-                  </div>
-                  <Link
-                    to="login"
-                    className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
-                  >
-                    Log in
-                  </Link>
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
+            )}
+          </nav>
           <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
             <div className="lg:grid lg:grid-cols-12 lg:gap-8">
               <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
