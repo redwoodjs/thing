6f4fa9a0255cca0bf26bfc5e1e3f461ccdf5ed88
A. David Thyresson <dthyresson@gmail.com>
2022-02-22 07:53:57 -0500

    Tests correct and wrong play guesses

--- api/src/services/games/games.test.ts
+++ api/src/services/games/games.test.ts
@@ -1,6 +1,11 @@
 import type { StandardScenario as GameStandardScenario } from './games.scenarios'
 
-import { possiblesForMovieId, randomMovie } from './games'
+import {
+  possiblesForMovieId,
+  randomMovie,
+  createNewGamePlay,
+  answerPlay,
+} from './games'
 
 describe('games', () => {
   // Note: This test uses a larger tablesample size in test than in dev or prod
@@ -25,6 +30,102 @@ describe('games', () => {
 
       expect(movies.length).toEqual(5)
       expect(movieIds).toContain(movieId)
+    }
+  )
+
+  scenario(
+    'Simulates a new game for a player with a correct movie and unanswered play',
+    async (scenario: GameStandardScenario) => {
+      const play = await createNewGamePlay()
+
+      const allMovieIds = [
+        scenario.movie.starman.id,
+        scenario.movie.escape.id,
+        scenario.movie.trouble.id,
+        scenario.movie.thing.id,
+        scenario.movie.precinct.id,
+        scenario.movie.christine.id,
+      ]
+
+      expect(play).toBeTruthy()
+      expect(play.playerId).toBeTruthy()
+      expect(allMovieIds).toContain(play.correctMovieId)
+      expect(play.answeredMovieId).toBeNull()
+      expect(play.correctness).toBeNull()
+    }
+  )
+
+  scenario(
+    'Simulates a correct answer',
+    async (scenario: GameStandardScenario) => {
+      const play = await createNewGamePlay()
+
+      const allMovieIds = [
+        scenario.movie.starman.id,
+        scenario.movie.escape.id,
+        scenario.movie.trouble.id,
+        scenario.movie.thing.id,
+        scenario.movie.precinct.id,
+        scenario.movie.christine.id,
+      ]
+
+      expect(play).toBeTruthy()
+      expect(play.playerId).toBeTruthy()
+      expect(allMovieIds).toContain(play.correctMovieId)
+      expect(play.answeredMovieId).toBeNull()
+      expect(play.correctness).toBeNull()
+
+      const answeredMovieId = play.correctMovieId
+
+      const answered = await answerPlay({
+        playId: play.id,
+        playerId: play.playerId,
+        answeredMovieId,
+      })
+
+      expect(answered).toBeTruthy()
+      expect(answered.playerId).toBeTruthy()
+      expect(allMovieIds).toContain(play.correctMovieId)
+      expect(answered.answeredMovieId).toEqual(answeredMovieId)
+      expect(answered.correctness).toEqual(true)
+    }
+  )
+
+  scenario(
+    'Simulates a wrong answer',
+    async (scenario: GameStandardScenario) => {
+      const play = await createNewGamePlay()
+
+      const allMovieIds = [
+        scenario.movie.starman.id,
+        scenario.movie.escape.id,
+        scenario.movie.trouble.id,
+        scenario.movie.thing.id,
+        scenario.movie.precinct.id,
+        scenario.movie.christine.id,
+      ]
+
+      expect(play).toBeTruthy()
+      expect(play.playerId).toBeTruthy()
+      expect(allMovieIds).toContain(play.correctMovieId)
+      expect(play.answeredMovieId).toBeNull()
+      expect(play.correctness).toBeNull()
+
+      const answeredMovieId = allMovieIds.filter(
+        (id) => id != play.correctMovieId
+      )[0]
+
+      const answered = await answerPlay({
+        playId: play.id,
+        playerId: play.playerId,
+        answeredMovieId,
+      })
+
+      expect(answered).toBeTruthy()
+      expect(answered.playerId).toBeTruthy()
+      expect(allMovieIds).toContain(play.correctMovieId)
+      expect(answered.answeredMovieId).toEqual(answeredMovieId)
+      expect(answered.correctness).toEqual(false)
     }
   )
 })
