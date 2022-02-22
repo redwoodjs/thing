1709d0771ad1c02b0c061095026394740242bf2f
A. David Thyresson <dthyresson@gmail.com>
2022-02-21 21:30:49 -0500

    Scores a play

--- scripts/moviesForGame.ts
+++ scripts/moviesForGame.ts
@@ -1,14 +1,14 @@
 // To access your database
 // Append api/* to import from api and web/* to import from web
-import { moviesForGame } from 'api/src/services/games/games'
+import { possiblesForMovieId } from 'api/src/services/games/games'
 
 export default async ({ args }) => {
   // Your script here...
   console.log(':: Executing script with args ::')
   console.log(args)
 
-  const movies = await moviesForGame({
-    id: '011de4c7-529c-4813-ad62-b964f09bce82',
+  const movies = await possiblesForMovieId({
+    movieId: '011de4c7-529c-4813-ad62-b964f09bce82',
   })
 
   console.log(movies)
