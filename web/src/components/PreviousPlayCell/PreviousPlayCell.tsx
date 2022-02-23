833a5bef10f567dcf82ced6883c8ec3b0495cd68
Tobbe Lundberg <tobbe@tlundberg.com>
2022-02-23 20:44:59 +0100

    PreviousPlayCell: Don't display empty or loading

--- web/src/components/PreviousPlayCell/PreviousPlayCell.tsx
+++ web/src/components/PreviousPlayCell/PreviousPlayCell.tsx
@@ -29,9 +29,9 @@ export const QUERY = gql`
   }
 `
 
-export const Loading = () => <div>Loading...</div>
+export const Loading = () => <div></div>
 
-export const Empty = () => <div>Empty</div>
+export const Empty = () => <div></div>
 
 export const Failure = ({ error }: CellFailureProps) => (
   <div style={{ color: 'red' }}>Error: {error.message}</div>
