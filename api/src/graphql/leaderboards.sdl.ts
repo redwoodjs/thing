127edcf433df45d69a7c70d0a899d142442f7bb4
A. David Thyresson <dthyresson@gmail.com>
2022-02-23 20:48:07 -0500

    leaderboard services

---
+++ api/src/graphql/leaderboards.sdl.ts
@@ -0,0 +1,17 @@
+export const schema = gql`
+  type Leaderboard {
+    playerId: String!
+    player: Player!
+    correctTotal: Int!
+    incorrectTotal: Int!
+    unansweredTotal: Int!
+    place: Int!
+    leaderboardRowNumber: Int!
+  }
+
+  type Query {
+    leaderboards: [Leaderboard!]! @skipAuth
+    playerLeaderboard(playerId: String!): Leaderboard @skipAuth
+    leaderboardWindow(playerId: String!, window: Int): [Leaderboard] @skipAuth
+  }
+`
