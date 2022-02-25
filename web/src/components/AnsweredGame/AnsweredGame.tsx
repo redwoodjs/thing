49a3d41ecf8cbc3053daf3aa604c6cb45a4310df
David Thyresson <dthyresson@gmail.com>
2022-02-25 10:05:01 -0500

    Change game play to show just answer (#30)

--- web/src/components/AnsweredGame/AnsweredGame.tsx
+++ web/src/components/AnsweredGame/AnsweredGame.tsx
@@ -6,7 +6,7 @@ import { getYear } from 'src/utils/date'
 //   play: Play | undefined
 // }
 
-const AnsweredGame = ({ play }) => {
+const AnsweredGame = ({ play, showMovies = false }) => {
   if (!play) {
     return null
   }
@@ -15,44 +15,46 @@ const AnsweredGame = ({ play }) => {
     <div>
       <h2 className="text-center">
         Which movie was released in{' '}
-        <span className="font-bold text-xl px-1">
+        <span className="font-bold text-lg px-1">
           {getYear(play.correctMovie.releasedOn)}
         </span>
         ?
       </h2>
-      <div>
-        <div className="flex items-center flex-1 justify-between w-full w-auto">
-          {play.possibleMovies.map(({ movie }) => {
-            return (
-              <div key={movie.id} className="w-40">
-                <div className="flex flex-col text-center ">
-                  <h3
-                    className={
-                      'py-4' +
-                      (movie.id === play.correctMovie.id
-                        ? ' font-bold text-green-700'
-                        : movie.id === play.answeredMovie.id
-                        ? ' font-bold text-red-700'
-                        : ' text-gray-500')
-                    }
-                  >
-                    {movie.title}
-                  </h3>
-                  <p className="font-bold text-center pb-2">
-                    {getYear(movie.releasedOn)}
-                  </p>
-                  <img
-                    className="drop-shadow-md"
-                    alt={movie.title}
-                    // Available poster sizes ['w92', 'w154', 'w185', 'w342', 'w500', 'w780', 'original']
-                    src={`https://image.tmdb.org/t/p/w185/${movie.photoPath}`}
-                  />
+      {showMovies && (
+        <div>
+          <div className="flex items-center flex-1 justify-between w-full w-auto">
+            {play.possibleMovies.map(({ movie }) => {
+              return (
+                <div key={movie.id} className="w-40">
+                  <div className="flex flex-col text-center ">
+                    <h3
+                      className={
+                        'py-4' +
+                        (movie.id === play.correctMovie.id
+                          ? ' font-bold text-green-700'
+                          : movie.id === play.answeredMovie.id
+                          ? ' font-bold text-red-700'
+                          : ' text-gray-500')
+                      }
+                    >
+                      {movie.title}
+                    </h3>
+                    <p className="font-bold text-center pb-2">
+                      {getYear(movie.releasedOn)}
+                    </p>
+                    <img
+                      className="drop-shadow-md"
+                      alt={movie.title}
+                      // Available poster sizes ['w92', 'w154', 'w185', 'w342', 'w500', 'w780', 'original']
+                      src={`https://image.tmdb.org/t/p/w185/${movie.photoPath}`}
+                    />
+                  </div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })}
+          </div>
         </div>
-      </div>
+      )}
       <div className="mt-4">
         {play.correctness && (
           <div>
@@ -66,7 +68,10 @@ const AnsweredGame = ({ play }) => {
                 />
               </div>
               <div className="col-span-3">
-                <h5 className="text-lg font-semibold mb-2">
+                <p className="text-lg font-bold mb-1">
+                  {getYear(play.correctMovie.releasedOn)}
+                </p>
+                <h5 className="text-lg font-semibold mb-1">
                   {play.correctMovie.title}
                 </h5>
                 <p>{play.correctMovie.overview}</p>
@@ -86,7 +91,10 @@ const AnsweredGame = ({ play }) => {
                 />
               </div>
               <div className="col-span-3">
-                <h5 className="text-lg font-semibold mb-2">
+                <p className="text-lg font-bold mb-1">
+                  {getYear(play.answeredMovie.releasedOn)}
+                </p>
+                <h5 className="text-lg font-semibold mb-1">
                   {play.answeredMovie.title}
                 </h5>
                 <p>{play.answeredMovie.overview}</p>
@@ -101,7 +109,10 @@ const AnsweredGame = ({ play }) => {
                 />
               </div>
               <div className="col-span-3">
-                <h5 className="text-lg font-semibold mb-2">
+                <p className="text-lg font-bold mb-1">
+                  {getYear(play.correctMovie.releasedOn)}
+                </p>
+                <h5 className="text-lg font-semibold mb-1">
                   {play.correctMovie.title}
                 </h5>
                 <p>{play.correctMovie.overview}</p>
