4abe120674fdc3536fae99e4704edf84f4604a52
A. David Thyresson <dthyresson@gmail.com>
2022-02-25 09:43:12 -0500

    More leader stories

--- web/src/components/Leaderboard/Leaderboard.stories.tsx
+++ web/src/components/Leaderboard/Leaderboard.stories.tsx
@@ -5,4 +5,31 @@ export const generated = () => {
   return <Leaderboard leaderboard={standard().leaderboard} playerId={null} />
 }
 
+export const top = () => {
+  return (
+    <Leaderboard
+      leaderboard={standard().leaderboard.splice(0, 4)}
+      playerId={null}
+    />
+  )
+}
+
+export const middle = () => {
+  return (
+    <Leaderboard
+      leaderboard={standard().leaderboard.splice(5, 5)}
+      playerId={null}
+    />
+  )
+}
+
+export const tied = () => {
+  return (
+    <Leaderboard
+      leaderboard={standard().leaderboard.splice(3, 5)}
+      playerId={null}
+    />
+  )
+}
+
 export default { title: 'Components/Leaderboard' }
