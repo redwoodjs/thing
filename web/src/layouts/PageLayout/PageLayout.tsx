5c04045e302ad0487251dc64572499b4f814389b
Tobbe Lundberg <tobbe@tlundberg.com>
2022-03-26 22:06:30 +0100

    New logo

--- web/src/layouts/PageLayout/PageLayout.tsx
+++ web/src/layouts/PageLayout/PageLayout.tsx
@@ -71,11 +71,7 @@ const PageLayout = ({ children }: PageLayoutProps) => {
             <div className="flex items-center justify-between w-full w-auto">
               <Link to={routes.home()}>
                 <span className="sr-only">Home</span>
-                <img
-                  className="h-8 w-auto sm:h-10"
-                  src="https://d33wubrfki0l68.cloudfront.net/492ed629970792d32ac857da0166a7d2308bad99/428b6/images/diecut.svg"
-                  alt=""
-                />
+                <img className="h-8 w-auto sm:h-10" src="./logo.png" alt="" />
               </Link>
             </div>
             <div className="ml-5 space-x-5 md:ml-10 md:space-x-10">
